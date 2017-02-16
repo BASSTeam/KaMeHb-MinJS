6bf63901e12ab540fda3d6da00e44031cd34bd66
BASSTeam <blade.whiteblack@gmail.com>
2017-02-16 23:11:41 +0200

    Fixed so many bugs

--- debug_console.php
+++ debug_console.php
@@ -1,26 +1,34 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+//require('https://raw.githubusercontent.com/BASSTeam/KaMeHb-MinJS/master/debug_console.php');
 class debugConsole{
 	private $state = true;
 	private $out = "";
-	private function addElement($tagname, $text, $id, $name, $js, $css){
-		$out += "<$tagname" . ($id !== NULL ? " id=\"$id\"" : "") . ($name !== NULL ? " name=\"$name\"" : "") . ">$text</$tagname>";
+	private function addElement($tagname, $text = "", $id = NULL, $name = NULL, $js = NULL, $css = NULL){
+		$this -> $out += "<$tagname" . ($id !== NULL ? " id=\"$id\"" : "") . ($name !== NULL ? " name=\"$name\"" : "") . ">$text</$tagname>";
 	}
-	public function setOn(){ $state = true;}
-	public function setOff(){ $state = false;}
-	public function setReverseState(){ $state = !$state;}
-	public function setPOST($json){ $_POST = json_decode($json) -> post;}
-	public function setGET($json){ $_POST = json_decode($json) -> get;}
-	public function message($message){ $out += addElement("div", $message, NULL, "message");}
+	public function turnOn(){ $this -> state = true;}
+	public function turnOff(){ $this -> state = false;}
+	public function setReverseState(){ $this -> state = !($this -> state);}
+	public function setPOST($json){ $_POST = json_decode($json);}
+	public function setGET($json){ $_POST = json_decode($json);}
+	public function message($message){ $this -> $out += $this -> addElement("div", $message, NULL, "message");}
 	public function getArgs(){
-		if ($state){
-			addElement("div", json_encode($_POST), NULL, "POST");
-			addElement("div", json_encode($_GET), NULL, "GET");
+		if ($this -> state){
+			$this -> addElement("div", json_encode($_POST), NULL, "POST");
+			$this -> addElement("div", json_encode($_GET), NULL, "GET");
 		}
 	}
 	public function construct(){
-		if ($state){
+		if ($this -> state){
+			$out = $this -> out;
 			return "<div id=\"debugConsole\" style=\"\">$out</div>";
 		}
 	}
 }
-?>
+$debugConsole = new debugConsole();
+$debugConsole -> getArgs();
+echo $debugConsole -> construct();
+?> 
