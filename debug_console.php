75331285cea86e7c3b950382e34f0e89d780d225
BASSTeam <blade.whiteblack@gmail.com>
2017-02-16 23:22:26 +0200

    Fixed encountering a non-numeric value

--- debug_console.php
+++ debug_console.php
@@ -3,14 +3,14 @@ class debugConsole{
 	private $state = true;
 	private $out = "";
 	private function addElement($tagname, $text = "", $id = NULL, $name = NULL, $js = NULL, $css = NULL){
-		$this -> out += "<$tagname" . ($id !== NULL ? " id=\"$id\"" : "") . ($name !== NULL ? " name=\"$name\"" : "") . ">$text</$tagname>";
+		$this -> out = $this -> out . "<$tagname" . ($id !== NULL ? " id=\"$id\"" : "") . ($name !== NULL ? " name=\"$name\"" : "") . ">$text</$tagname>";
 	}
 	public function turnOn(){ $this -> state = true;}
 	public function turnOff(){ $this -> state = false;}
 	public function setReverseState(){ $this -> state = !($this -> state);}
 	public function setPOST($json){ $_POST = json_decode($json);}
 	public function setGET($json){ $_POST = json_decode($json);}
-	public function message($message){ $this -> $out += $this -> addElement("div", $message, NULL, "message");}
+	public function message($message){ $this -> addElement("div", $message, NULL, "message");}
 	public function getArgs(){
 		if ($this -> state){
 			$this -> addElement("div", json_encode($_POST), NULL, "POST");
