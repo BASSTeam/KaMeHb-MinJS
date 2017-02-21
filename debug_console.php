56f9fbceebe72127c6f088d684858856dde8b2c7
BASSTeam <blade.whiteblack@gmail.com>
2017-02-21 11:01:54 +0200

    fixed getting property of undefined in js

--- debug_console.php
+++ debug_console.php
@@ -124,13 +124,13 @@ class debugConsole{
 			echo $out;
 			echo "<table style=\"width:100%;color:inherit;font:inherit;font-size:inherit;border-collapse:collapse;border:0;\"><tbody><tr><td style=\"width:1px;border:0;\">$codename@$server:$</td><td style=\"overflow:hidden;border:0;\"><div style=\"height: 18px;\">";
 			echo "<form method=\"POST\" name=\"submitform\" style=\"width:100%;\" onsubmit=\"
-			var c_value = this.name;
+			var c_value = this.querySelector('input[name=command_for_debug_console]').value;
 			alert(c_value);
 			var expression = '(';
 			var pos = c_value.search(expression);
 			if (pos != -1){
-				this.querySelector('input[name=command_type_for_debug_console]')[0].value = 'function';
-				c_value = string.substr(0, pos);
+				this.querySelector('input[name=command_type_for_debug_console]').value = 'function';
+				this.querySelector('input[name=command_for_debug_console]').value = string.substr(0, pos);
 				alert(pos);
 			}
 			submit(this);
