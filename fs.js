b1f63ae2abd6229e29998540d2d42393dfc36b2d
BASSTeam <blade.whiteblack@gmail.com>
2017-06-14 00:23:23 +0300

    Added require function prototype

--- fs.js
+++ fs.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
     Author:     Влад KaMeHb Марченко
     Version:    1.0-f
     ToDo:       --
@@ -38,4 +38,40 @@ document.addEventListener("DOMContentLoaded", function(){
             return result;
         }
     };
+    function getXmlHttp(){
+        var xmlhttp;
+        try{
+            xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch(e){
+            try{
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch(E){
+                xmlhttp = false;
+            }
+        }
+        if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+            xmlhttp = new XMLHttpRequest();
+        }
+        return xmlhttp;
+    }
+    require = function(fname, returnResult = false){
+        if(document.querySelector('script[_data-url="' + fname + '"]') == null){
+            var xhr = getXmlHttp();
+            xhr.open('GET', fname, false);
+            xhr.send();
+            if (xhr.status != 200){
+                console.error('Error loading ' + fname + ' (' + xhr.status + ': ' + xhr.statusText + ')\n\tat ' + fs.getCurrentFName() + ':60:8');
+                return false;
+            } else {
+                if (returnResult) return xhr.responseText;
+                var tmp = document.createElement('script');
+                tmp.innerHTML = xhr.responseText;
+                tmp.setAttribute('_data-url', fname);
+                document.head.appendChild(tmp);
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
 });
